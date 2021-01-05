0bbd3162ae26990b039e8403238a7a4c1325a361
Radmir Sultamuratov <sultamuratov.r@gmail.com>
2021-01-05 13:10:42 -0600

    Good so far

--- aops_bot.py
+++ aops_bot.py
@@ -1,11 +1,11 @@
 import discord
-from search_tool import search
+from search_tool import *
 
 client = discord.Client()
 with open('token.txt', 'r') as reader:
     token = reader.read()
 
-amcs, years, problems, key_words = None, None, None, None
+amc = years = problems = key_words = None
 
 
 @client.event
@@ -15,33 +15,37 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    global amcs, years, problems, key_words
+    global amc, years, problems, key_words
     if message.author == client.user:
         return
 
     if message.content.startswith('Hello'):
-        await message.channel.send("A - amc classes \nY - years \nP - problems range \nK - key words \nS - search")
+        await message.channel.send("A - amc classes \nY - years range \nP - problems range \nS - search")
 
     if message.content.startswith('A') or message.content.startswith('a'):
-        amcs = message.content.split()[1:]
-        await message.channel.send("Ok...")
+        amc = message.content.split()[1]
+        await message.channel.send("Ok.")
 
     if message.content.startswith('Y') or message.content.startswith('y'):
-        years = message.content.split()[1:]
-        await message.channel.send("Ok...")
+        years = message.content.split()
+        await message.channel.send("Ok.")
 
     if message.content.startswith('P') or message.content.startswith('p'):
-        problems = message.content.split()[1:]
-        await message.channel.send("Ok...")
-
-    if message.content.startswith('K') or message.content.startswith('k'):
-        key_words = message.content.split()[1:]
-        await message.channel.send("Ok...")
+        problems = message.content.split()
+        await message.channel.send("Ok.")
 
     if message.content.startswith('S') or message.content.startswith('s'):
-        if amcs and years:
-            results = search(amcs, years, problems, key_words)
-            await message.channel.send(results)
+        if amc and years:
+            y1 = int(years[1])
+            y2 = int(years[2])
+            try:
+                p1 = int(problems[1])
+                p2 = int(problems[2])
+            except:
+                p1 = 1
+                p2 = 25
+
+
 
 
 client.run(token)
--- search_tool.py
+++ search_tool.py
@@ -1,2 +1,57 @@
-def search(amcs, years, problems, key_words):
-    return None
+from bs4 import Tag, NavigableString, BeautifulSoup
+import requests
+from datetime import datetime
+
+
+def stop_tag(tag):
+    if tag.has_attr("id"):
+        return tag["id"] == "Solution_1" or tag["id"] == "Solution"
+    return False
+
+
+def get_text(tag):
+    tag.get_text(strip=True).split()
+
+
+time_now = datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+
+
+def write_txt_file(problem_set):
+    time_now = datetime.now().strftime("%m-%d-%Y %H-%M-%S")
+    with open("problem set " + time_now + ".txt", "w") as file:
+        file.write("Problem Set " + time_now + "\\vspace{3mm}\n\n")
+        file.writelines(problem_set)
+
+
+def get_problem(amc="8", year=14, problem_num=15):
+    year_str = str(year) if year>9 else "0"+str(year)
+    link = "https://artofproblemsolving.com/wiki/index.php/20" + year_str + "_AMC_" + amc + "_Problems/Problem_" + str(
+        problem_num)
+    with requests.get(link) as req:
+        soup = BeautifulSoup(req.content, features="html.parser")
+    problem = []
+    start, stop = soup.find("p"), soup.find(stop_tag)
+    while start != stop:
+        if isinstance(start, NavigableString):
+            problem.append(start)
+        elif isinstance(start, Tag) and start.name == 'img' and start.has_attr("alt"):
+            if start['alt'][:5] == "[asy]":
+                problem.append('\n\\href{' + start['src'] + '}{Picture}\n')
+            else:
+                problem.append(start['alt'])
+        start = start.next_element
+    return "20" + year_str + " AMC " + amc + " Problem " + str(problem_num) + " \\newline \\indent " + "".join(
+        problem) + "\\vspace{3mm}"
+
+
+def get_problem_set(amc, y1, y2, p1=1, p2=25):
+    problem_set = []
+    for y in range(y1, y2 + 1):
+        for p in range(p1, p2 + 1):
+            problem_set.append(get_problem(amc, y, p))
+    return problem_set
+
+
+if __name__ == "__main__":
+#    write_txt_file(get_problem_set("8", 10, 15,2, 4))
+    print(get_problem(amc="8", year=19, problem_num=1))
