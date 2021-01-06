42411f273c812d08944b0023e63a53b7830c252c
Radmir Sultamuratov <sultamuratov.r@gmail.com>
2021-01-05 23:18:10 -0600

    So good so far

--- aops_bot.py
+++ aops_bot.py
@@ -1,10 +1,11 @@
 import discord
+from parse_and_store import store_problem_set
 
 client = discord.Client()
 with open('token.txt', 'r') as reader:
     token = reader.read()
 
-amc = years = problems = key_words = None
+amc = y1 = y2 = p1 = p2 = None
 
 
 @client.event
@@ -14,20 +15,38 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    global amc, years, problems, key_words
+    global amc, y1, y2, p1, p2
     if message.author == client.user:
         return
-    else:
-        await message.channel.send("Hello! I am AoPS Bot.\n I can pull a problem or a problem set for you.\n 1 - problem\n 2 - problem set"
 
+    if message.content.lower().startswith('$hey'):
+        await message.channel.send(
+            "Hey! I am AoPS Bot. Do you need a problem set? \nWhich AMC are you intersted in? \nType: amc 8 (or amc 10a, ..., amc 12b)")
 
+    if message.content.lower() in ("amc 8", "amc 10a", "amc 10b", "amc 12a", "amc 12b"):
+        amc = message.content.split()[1]
+        await message.channel.send(
+            'Great. What period of are you interested in?\n(min: 1999, max:2020)\nExample: year 2007 2013')
 
+    if message.content.lower().startswith('year') and message.content.count(' ') == 2:
+        if message.content.split()[1].isnumeric() and message.content.split()[2].isnumeric():
+            if int(message.content.split()[1]) in range(2000, 2021) and int(message.content.split()[2]) in range(2000,
+                                                                                                                 2021):
+                y1 = int(message.content.split()[1])
+                y2 = int(message.content.split()[2])
+                await message.channel.send('Now. Problem numbers range?\n(min: 1, max: 25)\nExample: range 3 21')
 
-    if message.content.startswith('A') or message.content.startswith('a'):
-        await message.channel.send('Ok.')
+    if message.content.lower().startswith('range') and message.content.count(' ') == 2:
+        if message.content.split()[1].isnumeric() and message.content.split()[2].isnumeric():
+            if int(message.content.split()[1]) in range(1, 26) and int(message.content.split()[2]) in range(1, 26):
+                p1 = int(message.content.split()[1])
+                p2 = int(message.content.split()[2])
+                await message.channel.send("Type 'gen' to generate the problem set")
 
-
-
+    if message.content.lower() == "gen":
+        file_names = store_problem_set(amc="8", y1=2018, y2=2020, p1=3, p2=5)
+        my_files = [discord.File(name) for name in file_names]
+        await message.channel.send(files=my_files)
 
 
 client.run(token)
--- parse_and_store.py
+++ parse_and_store.py
@@ -10,64 +10,72 @@ def save_image_file(image_url):
 
 
 def insert_image(image_url):
-    return "\\begin{figure}[h]\\centering\\includegraphics[height=4cm]{" + image_url[-35:] + "}\\end{figure}\n\n"
-
-
-def stop_tag(tag):
-    if tag.has_attr("id"):
-        return tag["id"] == "Solution_1" or tag["id"] == "Solution"
-    return False
+    return "\\begin{figure}[h]\\centering\\includegraphics[height=3cm]{" + image_url[-35:] + "}\\end{figure}\n\n"
 
 
 def get_text(tag):
     tag.get_text(strip=True).split()
 
 
-def store_problems(problem_set):
-    time_now = datetime.now().strftime("%m-%d-%Y %H-%M-%S")
-    with open("problem set " + time_now + ".txt", "w") as file:
-        file.write("Problem Set " + time_now + "\\vspace{3mm}\n\n")
-        file.writelines(problem_set)
-
-
-def get_problem(amc="8", year=14, problem_num=15):
-    link = f"https://artofproblemsolving.com/wiki/index.php/20{year:02}_AMC_{amc}_Problems/Problem_{problem_num}"
+def get_problem(amc="8", year=2014, problem_num=15):
+    link = f"https://artofproblemsolving.com/wiki/index.php/{year}_AMC_{amc}_Problems/Problem_{problem_num}"
     with requests.get(link) as req:
         soup = BeautifulSoup(req.content, features="html.parser")
     problem = []
     images_links = []
-    start, stop = soup.find("p"), soup.find(stop_tag)
-    while start != stop:
-        if isinstance(start, NavigableString):
+    start = soup.find("p")
+    while True:
+        if isinstance(start, NavigableString) and start != "\n":
             problem.append(start)
         elif isinstance(start, Tag) and start.name == 'img' and start.has_attr("alt"):
             if start['alt'].startswith("[asy]"):
                 problem.append(insert_image(start['src']))
-                print(start['src'])
                 images_links.append("http:" + start['src'])
             else:
                 problem.append(start['alt'])
         start = start.next_element
-    return f"20{year:02} AMC {amc} Problem {problem_num} \\newline \\indent " + "".join(
-        problem) + "\\vspace{3mm}", images_links
+        if isinstance(start, Tag):
+            if start.has_attr("id"):
+                if start["id"] == "Solution_1" or start["id"] == "Solution" or start["id"] == "toc":
+                    break
+    problem.insert(-1, "\\newline \\indent")
+    return f"{year} AMC {amc} Problem {problem_num} \\newline \\indent " + "".join(
+        problem) + "\\vspace{3mm}\n\n", images_links
 
 
-def get_problem_set(amc, y1, y2, p1=1, p2=25):
+def get_problem_set(amc="8", y1=2018, y2=2020, p1=3, p2=5):
     problem_set = []
+    images_links = []
     for y in range(y1, y2 + 1):
         for p in range(p1, p2 + 1):
-            problem_set.append(get_problem(amc, y, p))
-    return problem_set
+            problem, links = get_problem(amc, y, p)
+            problem_set.append(problem)
+            images_links.extend(links)
+    return problem_set, images_links
 
 
-def store_a_problem(amc="8", year=12, problem_num=1):
-    problem, image_links = get_problem(amc="8", year=14, problem_num=15)
+def store_a_problem(amc="8", year=2012, problem_num=1):
+    problem, image_links = get_problem(amc="8", year=2014, problem_num=15)
     for image_url in image_links:
         save_image_file(image_url)
-    with open(f"20{year:02} AMC {amc} Problem {problem_num}.txt", "w") as file:
-        file.write(f"20{year:02} AMC {amc} Problem {problem_num}" + "\\vspace{3mm}\n\n")
+    with open(f"{year} AMC {amc} Problem {problem_num}.txt", "w") as file:
+        file.write(f"{year} AMC {amc} Problem {problem_num}" + "\\vspace{3mm}\n\n")
         file.writelines(problem)
 
 
+def store_problem_set(amc="8", y1=2018, y2=2020, p1=3, p2=5):
+    problem_set, images_links = get_problem_set(amc, y1, y2, p1, p2)
+    time_now = datetime.now().strftime("%m-%d-%Y %H-%M-%S")
+    file_name = "problem set " + time_now + ".txt"
+    with open(file_name, "w") as file:
+        file.write("Problem Set " + time_now + "\\vspace{3mm}\n\n")
+        file.writelines(problem_set)
+    for url in images_links:
+        save_image_file(url)
+    file_names = [file_name]
+    file_names.extend([url[-35:] for url in images_links])
+    return file_names
+
+
 if __name__ == "__main__":
-    store_a_problem()
+    print(store_problem_set(amc="8", y1=2018, y2=2020, p1=3, p2=5))
