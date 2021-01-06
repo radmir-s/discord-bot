1a4de8d5e5c59148bc0153baecde0ad8f80e6e9e
Radmir Sultamuratov <sultamuratov.r@gmail.com>
2021-01-06 10:04:56 -0600

    carried all to functions to bot.py

--- aops_bot.py
+++ aops_bot.py
@@ -1,5 +1,6 @@
 import discord
 from parse_and_store import prepare_zip
+from os import remove
 
 client = discord.Client()
 with open('token.txt', 'r') as reader:
@@ -23,30 +24,36 @@ async def on_message(message):
         await message.channel.send(
             "Hey! I am AoPS Bot. Do you need a problem set? \nWhich AMC are you intersted in? \nType: amc 8 (or amc 10a, ..., amc 12b)")
 
-    if message.content.lower() in ("amc 8", "amc 10a", "amc 10b", "amc 12a", "amc 12b"):
-        amc = message.content.split()[1]
+    if message.content.upper() in ("AMC 8", "AMC 10A", "AMC 10B", "AMC 12A", "AMC 12B"):
+        amc = message.content.upper().replace(" ","_")
         await message.channel.send(
             'Great. What period of are you interested in?\n(min: 1999, max:2020)\nExample: year 2007 2013')
 
     if message.content.lower().startswith('year') and message.content.count(' ') == 2:
-        if message.content.split()[1].isnumeric() and message.content.split()[2].isnumeric():
-            if int(message.content.split()[1]) in range(2000, 2021) and int(message.content.split()[2]) in range(2000,
-                                                                                                                 2021):
-                y1 = int(message.content.split()[1])
-                y2 = int(message.content.split()[2])
-                await message.channel.send('Now. Problem numbers range?\n(min: 1, max: 25)\nExample: range 3 21')
+        y1, y2 = message.content.split()[1:3]
+        if y1.isnumeric() and y2.isnumeric():
+            y1 = max(1999, int(y1))
+            y2 = min(2020, int(y2))
+            await message.channel.send('Now. Problem numbers range?\n(min: 1, max: 25)\nExample: range 3 21')
 
     if message.content.lower().startswith('range') and message.content.count(' ') == 2:
-        if message.content.split()[1].isnumeric() and message.content.split()[2].isnumeric():
-            if int(message.content.split()[1]) in range(1, 26) and int(message.content.split()[2]) in range(1, 26):
-                p1 = int(message.content.split()[1])
-                p2 = int(message.content.split()[2])
-                await message.channel.send("Type 'gen' to generate the problem set")
+        p1, p2 = message.content.split()[1:3]
+        if p1.isnumeric() and p2.isnumeric():
+            p1 = max(1, int(p1))
+            p2 = min(25, int(p2))
+            await message.channel.send("Type 'gen' to generate the problem set")
+
+    if p1 and amc:
+        if p1 < 2003 and amc != "AMC_8":
+            p1 = max(2000, p1)
+            p2 = max(2000, p2)
+            amc = "AMC_10" if amc[5] == "0" else "AMC_12"
 
     if message.content.lower() == "gen":
         print(amc, y1, y2, p1, p2)
         zip_file_name = prepare_zip(amc, y1, y2, p1, p2)
         await message.channel.send(file=discord.File(zip_file_name))
+        remove(zip_file_name)
 
 
 client.run(token)
