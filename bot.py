5c423d348f62496bd2e1af8693484062c2d003e2
Radmir Sultamuratov <sultamuratov.r@gmail.com>
2021-01-07 11:23:13 -0600

    add search with keywords

--- bot.py
+++ bot.py
@@ -5,21 +5,16 @@ from zipfile import ZipFile, ZIP_DEFLATED
 import discord
 from os import remove
 
-tail = 12
 
-
-def save_image_file(image_url):
+def save_image_file(image_url, tail=12):
     req = requests.get(image_url)
     with open(image_url[-tail:], 'wb') as f:
         f.write(req.content)
 
 
-def insert_image(image_url):
-    return "\\begin{figure}[h]\\centering\\includegraphics[height=3cm]{" + image_url[-tail:] + "}\\end{figure}\n\n"
-
-
-def get_text(tag):
-    tag.get_text(strip=True).split()
+def insert_image(image_url, tail=12, height="2.5cm"):
+    return "\\begin{figure}[h]\\centering\\includegraphics[height=" + height + "]{" + image_url[
+                                                                                      -tail:] + "}\\end{figure}\n\n"
 
 
 def get_problem(amc="AMC_8", year=2014, problem_num=15):
@@ -52,21 +47,51 @@ def get_problem(amc="AMC_8", year=2014, problem_num=15):
         problem) + "\\vspace{3mm}\n\n", images_links
 
 
-def get_problem_set(amc="AMC_8", y1=2018, y2=2020, p1=3, p2=5):
+def is_keyword_in(amc="AMC_8", year=2014, problem_num=15, all_true=True, keys=tuple()):
+    link = f"https://artofproblemsolving.com/wiki/index.php/{year}_{amc}_Problems/Problem_{problem_num}"
+    with requests.get(link) as req:
+        soup = BeautifulSoup(req.content, features="html.parser")
+    if all_true:
+        for key in keys:
+            all_true = key in soup.find("p").text and all_true
+    else:
+        for key in keys:
+            all_true = key in soup.find("p").text or all_true
+
+    return all_true
+
+
+def probs_with_keywords(amc="AMC_8", y1=2005, y2=2007, p1=1, p2=3, all_true=True, keys=tuple()):
+    probs_with_keys = []
+    for y in range(y1, y2 + 1):
+        for p in range(p1, p2 + 1):
+            if is_keyword_in(amc, y, p, all_true, keys):
+                probs_with_keys.append((amc, y, p))
+    return probs_with_keys
+
+
+def get_problem_set_keys(probs_with_keys):
+    problem_set = []
+    images_links = []
+    for amc, y, p in probs_with_keys:
+        problem, links = get_problem(amc, y, p)
+        problem_set.append(problem)
+        images_links.extend(links)
+    return problem_set, images_links
+
+
+def get_problem_set_range(amc="AMC_8", y1=2018, y2=2020, p1=3, p2=5):
     problem_set = []
     images_links = []
     for y in range(y1, y2 + 1):
-        print(y)
         for p in range(p1, p2 + 1):
-            print(p)
             problem, links = get_problem(amc, y, p)
             problem_set.append(problem)
             images_links.extend(links)
     return problem_set, images_links
 
 
-def store_problem_set(amc="AMC_8", y1=2018, y2=2020, p1=3, p2=5):
-    problem_set, images_links = get_problem_set(amc, y1, y2, p1, p2)
+def store_problem_set(problem_set, images_links, tail=12):
     time_now = datetime.now().strftime("%m-%d-%Y %H-%M-%S")
     file_name = "problem set " + time_now + ".txt"
     with open(file_name, "w") as file:
@@ -79,8 +104,7 @@ def store_problem_set(amc="AMC_8", y1=2018, y2=2020, p1=3, p2=5):
     return file_names
 
 
-def prepare_zip(amc="AMC_8", y1=2018, y2=2020, p1=3, p2=5):
-    file_names = store_problem_set(amc, y1, y2, p1, p2)
+def prepare_zip(file_names):
     zip_file_name = file_names[0][:-4] + ".zip"
     zip_file = ZipFile(zip_file_name, "w")
     for file_name in file_names:
@@ -93,7 +117,7 @@ client = discord.Client()
 with open('token.txt', 'r') as reader:
     token = reader.read()
 
-amc = y1 = y2 = p1 = p2 = None
+amc = y1 = y2 = p1 = p2 = keys = all_true = None
 
 
 @client.event
@@ -103,11 +127,12 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    global amc, y1, y2, p1, p2
+    global amc, y1, y2, p1, p2, keys, all_true
     if message.author == client.user:
         return
 
     if message.content.lower().startswith('hey'):
+        amc = y1 = y2 = p1 = p2 = keys = all_true = None
         await message.channel.send(
             "Hey! Type an AMC you are interested in. \nExample: amc 8 (or amc 10a, ..., amc 12b)")
 
@@ -128,7 +153,8 @@ async def on_message(message):
         if p1.isnumeric() and p2.isnumeric():
             p1 = max(1, int(p1))
             p2 = min(25, int(p2))
-            await message.channel.send("Type 'gen' to generate the problem set")
+            await message.channel.send(
+                "Type 'nokeys', 'keys all' or 'keys any' with some keywords.\nExamples:\nnokeys\nkeys all triangle vertex\nkeys any triangle vertex")
 
     if y1 and amc:
         if y1 < 2003 and amc != "AMC_8":
@@ -136,12 +162,33 @@ async def on_message(message):
             y2 = min(2002, y2)
             amc = "AMC_10" if "10" in amc else "AMC_12"
 
+    if message.content.lower().startswith('nokeys'):
+        keys = None
+        expect = (y2 - y1 + 1) * (p2 - p1 + 1) * 2
+        await message.channel.send(f"Type 'gen' to generate the problem set.\nExpect a zip file in {expect} sec.")
+
+    if message.content.lower().startswith('keys'):
+        mssg = message.content.split()
+        all_true = True if mssg[1] == "all" else False
+        keys = mssg[2:]
+        expect = (y2 - y1 + 1) * (p2 - p1 + 1) * 2
+        await message.channel.send(f"Type 'gen' to generate the problem set.\nExpect a zip file in {expect} sec.")
+
     if message.content.lower() == "gen":
-        print(amc, y1, y2, p1, p2, message.author)
-        zip_file_name = prepare_zip(amc, y1, y2, p1, p2)
-        await message.channel.send(file=discord.File(zip_file_name))
-        remove(zip_file_name)
-        amc = y1 = y2 = p1 = p2 = None
+        if all((amc, p1, p2, y1, y2)):
+            print(amc, y1, y2, p1, p2, message.author)
+            if keys:
+                problem_set, images_links = get_problem_set_keys(
+                    probs_with_keywords(amc, y1, y2, p1, p2, all_true, keys))
+            else:
+                problem_set, images_links = get_problem_set_range(amc, y1, y2, p1, p2)
+            zip_file_name = prepare_zip(store_problem_set(problem_set, images_links))
+            await message.channel.send(file=discord.File(zip_file_name))
+            remove(zip_file_name)
+            amc = y1 = y2 = p1 = p2 = keys = all_true = None
+            await message.channel.send("Type 'hey' to start another search.")
+        else:
+            await message.channel.send("We are missing some search data. Type 'hey' to start again")
 
 
 client.run(token)
--- parse_and_store.py
+++ parse_and_store.py
@@ -4,21 +4,15 @@ from datetime import datetime
 from zipfile import ZipFile, ZIP_DEFLATED
 from os import remove
 
-tail = 12
-
-
-def save_image_file(image_url):
+def save_image_file(image_url, tail=12):
     req = requests.get(image_url)
     with open(image_url[-tail:], 'wb') as f:
         f.write(req.content)
 
 
-def insert_image(image_url):
-    return "\\begin{figure}[h]\\centering\\includegraphics[height=3cm]{" + image_url[-tail:] + "}\\end{figure}\n\n"
-
-
-def get_text(tag):
-    tag.get_text(strip=True).split()
+def insert_image(image_url, tail=12, height="2.5cm"):
+    return "\\begin{figure}[h]\\centering\\includegraphics[height=" + height + "]{" + image_url[
+                                                                                      -tail:] + "}\\end{figure}\n\n"
 
 
 def get_problem(amc="AMC_8", year=2014, problem_num=15):
@@ -51,7 +45,40 @@ def get_problem(amc="AMC_8", year=2014, problem_num=15):
         problem) + "\\vspace{3mm}\n\n", images_links
 
 
-def get_problem_set(amc="AMC_8", y1=2018, y2=2020, p1=3, p2=5):
+def is_keyword_in(amc="AMC_8", year=2014, problem_num=15, all_true=True, keys=tuple()):
+    link = f"https://artofproblemsolving.com/wiki/index.php/{year}_{amc}_Problems/Problem_{problem_num}"
+    with requests.get(link) as req:
+        soup = BeautifulSoup(req.content, features="html.parser")
+    if all_true:
+        for key in keys:
+            all_true = key in soup.find("p").text and all_true
+    else:
+        for key in keys:
+            all_true = key in soup.find("p").text or all_true
+
+    return all_true
+
+
+def probs_with_keywords(amc="AMC_8", y1=2005, y2=2007, p1=1, p2=3, all_true=True, keys=tuple()):
+    probs_with_keys = []
+    for y in range(y1, y2 + 1):
+        for p in range(p1, p2 + 1):
+            if is_keyword_in(amc, y, p, all_true, keys):
+                probs_with_keys.append((amc, y, p))
+    return probs_with_keys
+
+
+def get_problem_set_keys(probs_with_keys):
+    problem_set = []
+    images_links = []
+    for amc, y, p in probs_with_keys:
+        problem, links = get_problem(amc, y, p)
+        problem_set.append(problem)
+        images_links.extend(links)
+    return problem_set, images_links
+
+
+def get_problem_set_range(amc="AMC_8", y1=2018, y2=2020, p1=3, p2=5):
     problem_set = []
     images_links = []
     for y in range(y1, y2 + 1):
@@ -62,8 +89,7 @@ def get_problem_set(amc="AMC_8", y1=2018, y2=2020, p1=3, p2=5):
     return problem_set, images_links
 
 
-def store_problem_set(amc="AMC_8", y1=2018, y2=2020, p1=3, p2=5):
-    problem_set, images_links = get_problem_set(amc, y1, y2, p1, p2)
+def store_problem_set(problem_set, images_links, tail=12):
     time_now = datetime.now().strftime("%m-%d-%Y %H-%M-%S")
     file_name = "problem set " + time_now + ".txt"
     with open(file_name, "w") as file:
@@ -76,8 +102,7 @@ def store_problem_set(amc="AMC_8", y1=2018, y2=2020, p1=3, p2=5):
     return file_names
 
 
-def prepare_zip(amc="AMC_8", y1=2018, y2=2020, p1=3, p2=5):
-    file_names = store_problem_set(amc, y1, y2, p1, p2)
+def prepare_zip(file_names):
     zip_file_name = file_names[0][:-4] + ".zip"
     zip_file = ZipFile(zip_file_name, "w")
     for file_name in file_names:
@@ -87,4 +112,4 @@ def prepare_zip(amc="AMC_8", y1=2018, y2=2020, p1=3, p2=5):
 
 
 if __name__ == "__main__":
-    prepare_zip(amc="AMC_8", y1=2017, y2=2020, p1=1, p2=25)
+    pass
